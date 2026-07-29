31734602a434087c0268e02ac107c2e2e2d77410
Megavani <megavani.writer@gmail.com>
2026-07-29 18:02:59 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,35 @@
 
+import { auth } from "./firebase.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    const loginLink = document.getElementById("loginLink");
+    const registerLink = document.getElementById("registerLink");
+
+    if (!loginLink || !registerLink) return;
+
+    if (user) {
+
+        loginLink.style.display = "none";
+        registerLink.innerHTML = "Logout";
+
+        registerLink.onclick = async function (e) {
+
+            e.preventDefault();
+
+            await signOut(auth);
+
+            alert("Logged Out");
+
+            window.location.reload();
+
+        };
+
+    }
+
+});
