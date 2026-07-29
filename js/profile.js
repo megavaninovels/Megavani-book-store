79abbb491bf71cecf8e8aac393c365ed0013b3fd
Megavani <megavani.writer@gmail.com>
2026-07-29 22:24:00 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,40 @@
 
+import { auth, db } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+
+        window.location.href = "login.html";
+        return;
+
+    }
+
+    const docRef = doc(db, "users", user.uid);
+
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+        const data = docSnap.data();
+
+        document.getElementById("name").value = data.name || "";
+        document.getElementById("email").value = data.email || "";
+        document.getElementById("phone").value = data.phone || "";
+        document.getElementById("addressLine").value = data.addressLine || "";
+        document.getElementById("city").value = data.city || "";
+        document.getElementById("state").value = data.state || "";
+        document.getElementById("pincode").value = data.pincode || "";
+        document.getElementById("country").value = data.country || "India";
+
+    }
+
+});
