e5bef534c956c031cd9530a33c876283421ba46f
Megavani <megavani.writer@gmail.com>
2026-07-29 14:34:01 +0530

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,14 @@
 
+function getCart() {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+const cart = getCart();
+
+let total = 0;
+
+cart.forEach(book => {
+    total += book.price * book.quantity;
+});
+
+document.getElementById("checkout-total").innerText = "₹" + total;
