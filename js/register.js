69340bff3c3b24d4022883d58b55b7c7df18437d
Megavani <megavani.writer@gmail.com>
2026-07-29 17:26:52 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,40 @@
 
+import { auth } from "./firebase.js";
+
+import {
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const confirmPassword = document.getElementById("confirmPassword").value.trim();
+
+    if (!email || !password) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    try {
+
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Account Created Successfully");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
